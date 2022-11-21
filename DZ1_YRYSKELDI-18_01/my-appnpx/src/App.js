1860307f891c2a7dbdb51f9e3d317449dfd70c0a
Yryskeldi007 <112939262+Yryskeldi007@users.noreply.github.com>
2022-11-21 12:36:45 +0600

    Add files via upload

---
+++ DZ1_YRYSKELDI-18_01/my-appnpx/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+
+  // eslint-disable-next-line no-unused-vars
+  function AboutUs () {
+    return (
+      <div>
+      <h1>AboutUs</h1>
+      </div>
+    )
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  function Portfolio() {
+    return (
+      <>
+        <h1>Portfolio</h1>
+      </>
+    )
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  function Titlle () {
+    return <h1>titlle</h1>
+  }
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
+
